20e83d3acae0ba2a75ad3263dd5099a5a3755059
ABHISHEKGUPTA7788 <somugupta7788@gmail.com>
2023-11-19 01:27:01 +0530

    phir se

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,41 @@
-import {User} from '../Models/users.js'
+// import {User} from '../Models/users.js'
+// import jwt from 'jsonwebtoken'
+
+
+// export const isAuthenticated =  async(req,res,next) =>{
+//     const {token} = req.cookies
+
+//     if(!token) return res.status(404).json({
+//         success:false,
+//         message:"Please Login !!!! "
+//     })
+    
+//     const decode = jwt.verify(token,process.env.JWT_SECRET)
+    
+//     req.user = await User.findById(decode._id)
+
+//     next();
+// }
+import { User } from "../Models/users.js";
 import jwt from 'jsonwebtoken'
 
-
-export const isAuthenticated =  async(req,res,next) =>{
+export const isAuthenticated = async (req,res,next)=>{
     const {token} = req.cookies
+
+    console.log(token);
 
     if(!token) return res.status(404).json({
         success:false,
-        message:"Please Login !!!! "
+        message:"Please Login..!"
     })
-    
+
     const decode = jwt.verify(token,process.env.JWT_SECRET)
-    
+    // console.log("decoded data ",decode)
+
     req.user = await User.findById(decode._id)
+
+   
+    console.log(req.user);
     next();
+   
 }
